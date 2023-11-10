632fe5920a09b939444ddf95cb8936ca8dabb0aa
danbi <leedanbiii@naver.com>
2023-11-10 13:14:24 +0900

    view 환경설정

--- src/main/java/jpabook2/jpashop/Jpabook2Application.java
+++ src/main/java/jpabook2/jpashop/Jpabook2Application.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jpabook2Application {
 
     public static void main(String[] args) {
+
+        Hello hello = new Hello();
+        hello.setData("hello");
+        String data = hello.getData();
+        System.out.println("data = " + data);
+
         SpringApplication.run(Jpabook2Application.class, args);
     }
 
