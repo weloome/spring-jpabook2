e3d6df84967d809af5bc1b8537c61da9a2f4755a
danbi <leedanbiii@naver.com>
2023-11-10 10:04:08 +0900

    프로젝트 생성

---
+++ src/main/java/jpabook2/jpashop/Jpabook2Application.java
@@ -0,0 +1,13 @@
+package jpabook2.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpabook2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpabook2Application.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook2/jpashop/Jpabook2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook2.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpabook2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
