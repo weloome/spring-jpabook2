35c0164372c76e9b133576c48bca34d0535f1b86
danbi <leedanbiii@naver.com>
2023-11-11 00:43:47 +0900

    의존성 주입

---
+++ src/main/java/jpabook2/jpashop/domain/service/MemberService.java
@@ -0,0 +1,44 @@
+package jpabook2.jpashop.domain.service;
+
+import jpabook2.jpashop.domain.Member;
+import jpabook2.jpashop.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true) // 읽기 전용은 성능 최적화 효과
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // 회원 가입
+    @Transactional // default 값은 readOnly=false
+    public Long join(Member member) {
+        // 중복회원 검증
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) throw new IllegalStateException("이미 존재하는 회원입니다");
+    }
+
+    // 회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    // 회원 단 건 조회
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
