5895bf4cea547d52f26e78535a67520fd5834b46
danbi <leedanbiii@naver.com>
2023-11-11 02:49:07 +0900

    주문, 주문상품 엔티티

--- src/main/java/jpabook2/jpashop/domain/Order.java
+++ src/main/java/jpabook2/jpashop/domain/Order.java
@@ -1,9 +1,8 @@
 package jpabook2.jpashop.domain;
 
 import jakarta.persistence.*;
-import jpabook2.jpashop.domain.item.Item;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.Setter;ㅌ
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -46,5 +45,39 @@ public class Order {
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // 비즈니스 로직
+    public void cancel() { // 주문 취소
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // 조회 로직
+    public int getTotalPrice() { // 전체 주문 가격
+        // return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
