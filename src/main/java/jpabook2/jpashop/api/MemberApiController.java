471bd977abdce6002fdee81fc75d071f1b9a5e0f
danbi <leedanbiii@naver.com>
2023-11-18 22:33:27 +0900

    회원 조회 api

--- src/main/java/jpabook2/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook2/jpashop/api/MemberApiController.java
@@ -8,11 +8,28 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(member -> new MemberDto(member.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMember(@RequestBody @Valid Member member) {
@@ -29,10 +46,22 @@ public class MemberApiController {
     }
 
     @PutMapping("/api/v2/members/{id}")
-    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id, @RequestBody @Valid UpdateMemberRequest request) {
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
         memberService.update(id, request.getName());
         Member findMember = memberService.findOne(id);
         return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data @AllArgsConstructor
+    static class MemberDto { // DTO로 필요한것만 노출
+        private String name;
     }
 
     @Data
