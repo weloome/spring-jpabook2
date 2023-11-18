6ac313ab275e192badc8bc1b3b3a03ba3ac103de
danbi <leedanbiii@naver.com>
2023-11-19 02:33:42 +0900

    컬렉션 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- src/main/java/jpabook2/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook2/jpashop/api/OrderApiController.java
@@ -42,6 +42,18 @@ public class OrderApiController {
         return result;
     }
 
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        for (Order order : orders) {
+            System.out.println("order.getId() = " + order.getId());
+        }
+        List<OrderDto> result = orders.stream()
+                .map(o->new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
     @Getter
     static class OrderDto {
         private Long orderId;
