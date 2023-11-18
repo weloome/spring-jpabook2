0e2390a56f93df64126dbda5dd284162db63ebcb
danbi <leedanbiii@naver.com>
2023-11-19 00:38:48 +0900

    컬렉션 주문 조회 V1: 엔티티 직접 노출

---
+++ src/main/java/jpabook2/jpashop/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package jpabook2.jpashop.api;
+
+import jpabook2.jpashop.domain.Order;
+import jpabook2.jpashop.domain.OrderItem;
+import jpabook2.jpashop.repository.OrderRepository;
+import jpabook2.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAll(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
